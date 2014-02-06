c62e1aedb1923ca4fc5aef2db968397bbe0ab2e7
Scott Pidzarko <scott.pidzarko@gmail.com>
2014-02-05 16:07:22 -0800

    Began code for oven temp control
    control.asm sets or clears a bit on the basis of the read temperature
    vs. desired temperature.

---
+++ control.asm
@@ -0,0 +1,9 @@
+; CONTROL.ASM
+;
+; This file takes a Look-up table, provided in the file defined at the top of this piece of code,
+; as well as a temperature read and placed in R7, and sets or clears a bit in R0.0 to turn on or 
+; off the heater as desired
+;
+
+
+
